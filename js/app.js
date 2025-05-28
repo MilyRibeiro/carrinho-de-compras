1032dddf02f068c917678fa5faaa06d2c78bb579
Mylena Ribeiro <ribeiro.mylena1@gmail.com>
2025-05-28 14:21:32 -0300

    Verificando se o produto já foi adicionado ao carrinho

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
-// let totalGeral = 0;
-let totalGeral;
+let totalGeral = 0;
+let carrinhoDeCompras = [];
 // document.getElementById('lista-produtos').innerHTML = '';
 // document.getElementById('valor-total').textContent = 'R$ 0';
 limpar();
@@ -9,7 +9,7 @@ function adicionar() {
     let produto = document.getElementById('produto').value;
     let nomeDoProduto = produto.split('-')[0];
     let valorUnitario = parseFloat(produto.split('R$')[1]);
-    let quantidade = document.getElementById('quantidade').value;
+    let quantidade = parseInt(document.getElementById('quantidade').value);
     // alert(nomeDoProduto);
     // alert(valorUnitario);
     // alert(quantidade.value);
@@ -30,18 +30,37 @@ function adicionar() {
     let preco = parseFloat(quantidade * valorUnitario);
     // alert(preco);
 
+    // Verificar se o produto já está no carrinho:
+    let produtoJaAdicionado = carrinhoDeCompras.find(item => item.nome == nomeDoProduto);  // chamada de expressão lambda
+    if(produtoJaAdicionado) {
+        // Se o produto já estiver no carrinho, atualizar a quantidade e o preço:
+        produtoJaAdicionado.quantidade += quantidade;  // atualizar a quantidade do produto
+        produtoJaAdicionado.valor = produtoJaAdicionado.valor + preco;  // atualizar o valor total do produto
+    } else {
+        let carrinhoProdutos = {
+            nome: nomeDoProduto,
+            quantidade: quantidade,
+            valor: preco
+        };
+
+        carrinhoDeCompras.push(carrinhoProdutos)
+    }  // primeira vez que o produto é adicionado, ele não existe no carrinho.
+
     // adicionar no carrinho:
-    let carrinho = document.getElementById('lista-produtos');
-    carrinho.innerHTML += `
-        <section class="carrinho__produtos__produto">
-            <span class="texto-azul">${quantidade}x</span> ${nomeDoProduto} <span class="texto-azul">R$${preco}</span>
-        </section>
-    `;
+    let lista = document.getElementById('lista-produtos');
+    lista.innerHTML = '';  // limpar a lista antes de adicionar os produtos
+    carrinhoDeCompras.forEach(item => {
+        lista.innerHTML = lista.innerHTML + `
+            <section class="carrinho__produtos__produto">
+                <span class="texto-azul">${item.quantidade}x</span> ${item.nome} <span class="texto-azul">R$${item.valor.toFixed(2)}</span>
+            </section>
+        `;
+    })
 
     // atualizar o valor total:
-    totalGeral = totalGeral + preco;
+    totalGeral = parseFloat(totalGeral + preco);
     let campoTotal = document.getElementById('valor-total');
-    campoTotal.textContent = `R$ ${totalGeral}`;
+    campoTotal.textContent = `R$ ${totalGeral.toFixed(2)}`;
 
     // limpar o campo de quantidade:
     document.getElementById('quantidade').value = '';
@@ -49,6 +68,7 @@ function adicionar() {
 
 function limpar() {
     totalGeral = 0;
+    carrinhoDeCompras = [];
     document.getElementById('lista-produtos').innerHTML = '';
     document.getElementById('valor-total').textContent = 'R$ 0';
 }
